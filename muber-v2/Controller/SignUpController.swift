4a22cd6431bde80427e18e33fd29b443601aff27
bilaalukis <bilaalukis123@gmail.com>
2021-04-20 14:58:01 +0900

    login & signup bug fix

--- muber-v2/Controller/SignUpController.swift
+++ muber-v2/Controller/SignUpController.swift
@@ -137,7 +137,7 @@ class SignUpController: UIViewController {
                 
                 guard let controller = keyWindow?.rootViewController as? HomeController
                 else { return }
-                controller.configureUI()
+                controller.configure()
                 self.dismiss(animated: true, completion: nil)
             }
         }
